6c6c5a8e126564008453b25614c0956a342874be
Adithya Sirigiri <adithyasirigiri27@gmail.com>
2026-01-20 01:44:33 +0530

    Introduction to the SQL commands

---
+++ Beginner_SQL/01_Introduction.sql
@@ -0,0 +1,12 @@
+SELECT *
+FROM parks_and_recreation.employee_salary;
+
+SELECT first_name
+FROM parks_and_recreation.employee_salary;
+
+SELECT first_name,occupation
+FROM parks_and_recreation.employee_salary;
+
+#here you have to note that 
+# -->more than 2 queries are present then running flash button will give you both the outputs
+# -->when you only want one then simply hover it and use flash button +i or select it and use flash button
