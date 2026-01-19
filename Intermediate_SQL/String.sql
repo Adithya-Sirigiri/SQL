18b0aec486a585f730d116bbce4c58b2bed0de85
Adithya Sirigiri <adithyasirigiri27@gmail.com>
2026-01-20 01:55:27 +0530

    Strings in SQL

---
+++ Intermediate_SQL/String.sql
@@ -0,0 +1,18 @@
+SELECT first_name,length(last_name) AS lengthh
+FROM parks_and_recreation.employee_demographics
+WHERE first_name="Tom";
+
+SELECT length("Adithya") AS randomm;
+
+SELECT first_name,upper(last_name) AS lengthh
+FROM parks_and_recreation.employee_demographics;
+
+SELECT first_name,lower(last_name) AS lengthh
+FROM parks_and_recreation.employee_demographics;
+
+
+SELECT trim("  ADITHYA  ");
+
+SELECT ltrim("  ADITHYA  ");
+
+SELECT rtrim("  ADITHYA  ")
